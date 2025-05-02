9f9581d39ca3a74ae5267458851582e4738772ed
Niklas <nik_mey@uni-bremen.de>
2025-05-02 16:51:56 +0000

    Added initial structures for importing and exporting

---
+++ src/export/include/export_xml.h
@@ -0,0 +1,31 @@
+/**
+ *  Copyright 2025 Niklas Meyer
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * @file
+ * @author Niklas Meyer <nik_mey@uni-bremen.de>
+ *
+ * @section Description
+ *
+ * Functions to export xml files.
+ */
+
+#ifndef SDF_MULTI_CONVERTER_EXPORT_EXPORT_XML_H
+#define SDF_MULTI_CONVERTER_EXPORT_EXPORT_XML_H
+
+int ExportXmlFile();
+
+#endif //SDF_MULTI_CONVERTER_EXPORT_EXPORT_XML_H
